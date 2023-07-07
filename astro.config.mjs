99d608848b460a6cda97e9701345c31e980eb760
yatharth gupta <guptayatharth1@gmail.com>
2023-07-07 17:42:14 +0530

    checking typewriter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yatharth-gupta.github.io',
-    base: "/hosting/",
-    build: {
-        assets: 'astro'
-      }
+  site: 'https://yatharth-gupta.github.io',
+  base: "/hosting/",
+  build: {
+    assets: 'astro'
+  },
+  integrations: [react()]
 });
