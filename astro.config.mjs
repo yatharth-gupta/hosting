9dd7c10dae80a65d7d717d452752b4d760521ac7
yatharth gupta <guptayatharth1@gmail.com>
2023-07-07 12:08:29 +0530

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yatharth-gupta.github.io',
-    base: "/hosting"
+    base: "/hosting/"
 });
