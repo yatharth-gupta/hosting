bf6a5edabea3edd97dedeb9471e9ec97d35ee880
Shambhaviiiit <102795531+Shambhaviiiit@users.noreply.github.com>
2023-07-09 10:25:12 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,24 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // import typeWriter from 
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yatharth-gupta.github.io',
   base: "/hosting/",
+  output: "static",
   build: {
     assets: 'astro',
   },
-  vite: {
-    build:{
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/asset.[hash][extname]',
-        },
-      }
-    }
-  },
-  integrations: [react(), Typewriter()]
+  // vite: {
+  //   build:{
+  //     rollupOptions: {
+  //       output: {
+  //         assetFileNames: 'assets/asset.[hash][extname]',
+  //       },
+  //     }
+  //   }
+  // },
+  integrations: [react()]
 });
